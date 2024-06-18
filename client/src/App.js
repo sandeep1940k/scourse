c977225c60924c193051a43ce4df26d62d93a94f
sandeepsana9 <gondsgonds9@gmail.com>
2024-06-18 23:53:35 +0530

    signup design

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Signup from "./authentication/Signup";
 import LandingPage from "./LandingPage";
 import Login from "./authentication/Login";
 import Dashboard from "./pages/Dashboard";
+import Navigation from "./pages/Navigation";
 function App() {
   return (
     <>
-      <ToastContainer 
-        position="top-right" 
-        autoClose={5000} 
-        hideProgressBar={false} 
-        newestOnTop={false} 
-        closeOnClick 
-        rtl={false} 
-        pauseOnFocusLoss 
-        draggable 
-        pauseOnHover 
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
       />
-      <Router>
+      <BrowserRouter>
         <Routes>
+          {/* <Route path="/" element={<Navigation />}> */}
+            {/* <Route index element={<Dashboard />} /> */}
+            {/* <Route path="*" element={<NoPage />} /> */}
+          {/* </Route> */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/authentication/Signup.js
+++ client/src/authentication/Signup.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Signup.css';
-// import http from '../helper/http';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom"
 import { RESPONSE } from '../constant/response.constant';
+import background1 from "../images/background/background-1.jpg"
+import background2 from "../images/background/background-2.jpg"
 
 
 const Signup = () => {
     const navigate = useNavigate()
+    const [background, setBackground] = useState();
     const [formData, setFormData] = useState({
         username: '',
         password: '',
         email: '',
     });
+
+    useEffect(() => {
+        const randomBackground = Math.floor(Math.random() * 2) + 1;
+        setBackground(randomBackground === 1 ? background1 : background2);
+    }, []);
+
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -43,45 +51,42 @@ const Signup = () => {
     }
 
     return (
-        <div className="container">
-            <h2 className="heading">Signup</h2>
-            <form className="form" onSubmit={handleSubmit}>
-                <div className="formGroup">
-                    <label className="label">Username:</label>
-                    <input
-                        className="input"
-                        type="text"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                    />
+        <>
+            <div className='background'><img src={background} /></div>
+            <div className="card">
+                <img src={background} />
+                <div className='card-container'>
+                    <h1>sign up</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label >username</label><br />
+                        <input
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                        />
+                        <label >password</label><br />
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                        <label >email</label><br />
+                        <input
+                            type="text"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <p >you have already account - <span onClick={handleLogin}>login</span></p>
+                        <button type="submit" value="Signup" >
+                            sign up
+                        </button>
+                    </form>
                 </div>
-                <div className="formGroup">
-                    <label className="label">Password:</label>
-                    <input
-                        className="input"
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="formGroup">
-                    <label className="label">email:</label>
-                    <input
-                        className="input"
-                        type="text"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <p onClick={handleLogin}>Login</p>
-                <div>
-                    <input className="button" type="submit" value="Signup" />
-                </div>
-            </form>
-        </div>
+            </div>
+        </>
     );
 };
 
