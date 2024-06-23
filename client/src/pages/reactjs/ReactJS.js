107b04eddf102f5acc83a3c317b9244c26a5360b
sandeepsana9 <gondsgonds9@gmail.com>
2024-06-23 17:18:55 +0530

    new design

--- client/src/pages/reactjs/ReactJS.js
+++ client/src/pages/reactjs/ReactJS.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 import './ReactJS.css'
 
 const ReactJS = () => {
+    const location = useLocation();
     return (
         <>
             <nav>
                 <ul>
-                    <li>
+                    <li className={location.pathname === '/user/reactjs' ? 'active': ''}>
                         <Link to="/user/reactjs">Question-Answer</Link>
                     </li>
-                    <li>
+                    <li className={location.pathname === '/user/reactjs/mcq' ? 'active': ''}>
                         <Link to="/user/reactjs/mcq">MCQ</Link>
                     </li>
-                    <li>
+                    <li className={location.pathname === '/user/reactjs/coding-question' ? 'active': ''}>
                         <Link to="/user/reactjs/coding-question">Coding Question</Link>
                     </li>
                 </ul>
